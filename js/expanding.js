37809116039aa02ef6ad324f51b0b218a429766e
Abigail Weireter <111624254+AbiWeireter@users.noreply.github.com>
2023-03-16 13:39:05 -0700

    First Commit

---
+++ js/expanding.js
@@ -0,0 +1,21 @@
+const panels = document.querySelectorAll('.panel')
+
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        closePanel()
+        panel.classList.add('active')
+    })
+})
+
+
+
+
+function closePanel() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
+
+
+
