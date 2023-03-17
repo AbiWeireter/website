83e217417e7d44978631d8eb575435a71c6c65e4
Abigail Weireter <111624254+AbiWeireter@users.noreply.github.com>
2023-03-17 21:51:41 +0000

    Fri, Mar 17, 2023, 2:51 PM -07:00

---
+++ js/sound.js
@@ -0,0 +1,9 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'vicotry', 'wrong']
+
+sounds.forEach(sound =>  {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+    btn.innerText = sound
+
+    btn.addEvenListener('click', () =)
+})
