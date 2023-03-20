5d9b0da4fc8115c99eae31b42b153d111e802da6
Abigail Weireter <111624254+AbiWeireter@users.noreply.github.com>
2023-03-20 21:44:57 +0000

    Mon, Mar 20, 2023, 2:44 PM -07:00

--- js/jokes.js
+++ js/jokes.js
@@ -3,4 +3,11 @@ const jokeBtn = document.getElementById('jokeBtn')
 
 jokeBtn.addEventListener('click', generateJoke)
 
-function
+async function generateJoke(){
+    const response = await fetch('https://icanhazdadjoke.com', {
+        headers: {Accept: 'application/json'}
+    })
+
+    const data = await response.json()
+    jokeEl.innerHTML = data.joke
+}
